44ba55975c634d6860680836a6b73a515eec10ea
Alex Ivanov <aivanov@microsoft.com>
2021-02-04 18:31:29 -0500

    vNext

--- M02/Demo #3 DurableFunction/CSharp/DurFunction/Function1.cs
+++ M02/Demo #3 DurableFunction/CSharp/DurFunction/Function1.cs
@@ -2,38 +2,41 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace DurFunction
 {
-    public static class GetCities
+    public static class Functions
     {
         [FunctionName("GetCities")]
         public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] DurableOrchestrationContext context)
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
             var outputs = new List<string>();
 
+            // Replace "hello" with the name of your Durable Activity Function.
             outputs.Add(await context.CallActivityAsync<string>("ProcessCity", "Tokyo"));
             outputs.Add(await context.CallActivityAsync<string>("ProcessCity", "Seattle"));
             outputs.Add(await context.CallActivityAsync<string>("ProcessCity", "London"));
 
+            // returns ["Hello Tokyo!", "Hello Seattle!", "Hello London!"]
             return outputs;
         }
 
         [FunctionName("ProcessCity")]
         public static string SayHello([ActivityTrigger] string name, ILogger log)
         {
-            log.LogInformation($"Saying hello to {name}.");
+            log.LogInformation($"Saying hello to {name}");
             return $"Hello {name}!";
         }
 
-        [FunctionName("StartFunction")]
+        [FunctionName("HttpStart")]
         public static async Task<HttpResponseMessage> HttpStart(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]HttpRequestMessage req,
-            [OrchestrationClient]DurableOrchestrationClient starter,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
             ILogger log)
         {
             // Function input comes from the request content.
